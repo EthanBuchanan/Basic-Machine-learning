06203acb604fd24a9b0e31900200327c113cc415
Ethan Buchanan <Ethan@buchananville.com>
2018-09-10 17:47:23 -0600

    Added Saving capablities

--- AI_test.py
+++ AI_test.py
@@ -1,14 +1,22 @@
 import random
 import operator
+import copy
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import Element, SubElement, Comment, ElementTree
 
-mutationRate = 0.1
+# User changing variables (change as needed)
+
+
 fitnessReq = 0.1 # from 0 to 1 of how the tolerancy of what percentile is acceptable
-neuron = [4,4,2,1] # first number in this list should always match the number of inputs, and the last the number of outputs
-population = []
-evaluation = []
+mutationRate = 0.5
 populationSize = 100
-generationNum = 10000
+generationNum = 500
+neuron = [4,4,2,1] # first number in this list should always match the number of inputs, and the last the number of outputs
 
+
+
+file_Path = "XML_File_Test.xml" # the path to the file where generations are stored
+#make sure that if you change the values in the 'neuron' list that you also change the file_Path 
 
 InOut = [
     [[0,0,0,0],[0]],
@@ -42,6 +50,20 @@ InOut = [
     [[0,1,1,0],[1]],
     ]
 
+# declaration variables (don't touch)
+
+population = []
+evaluation = []
+try:
+    tree = ET.parse(file_Path)
+except:
+    test = """
+<data>
+</data>
+"""
+    tree = ElementTree(ET.fromstring(test))
+    tree.write(file_Path)
+
 def createNewGene():
     global neuron
     global genes
@@ -62,6 +84,7 @@ def createNewGene():
 
 
 def createGenes(Strain): # aka create mutated baby
+    
     prevStrain = Strain
     for layer in range(0,len(prevStrain)):
         for gen in range(0,len(prevStrain[layer])):
@@ -96,45 +119,96 @@ def decision(Strain,inpt):
         prevLayer = curLayer
         curLayer = []
     return prevLayer
-                
+
+def loadPrev():
+    global tree
+    root = tree.getroot()
+    
+    instances = populationSize
+    if len(root) < populationSize:
+        instances = len(root)
+    for saved in range(instances):
+        population[saved] = []
+        for layer in range(len(root[saved])):
+            entry = root[saved][layer].text
+            entry = entry.split()
+            entryL = list(map(float,entry))
+            population[saved].append(entryL)
+    
+def saveCur():
+    test = """
+<data>
+</data>
+"""
+    tree = ElementTree(ET.fromstring(test))
+    root = tree.getroot()
+    for saved in range(populationSize):
+        if population[saved] != 0:
+            ins = ET.Element('ins')
+            for layer in range(len(population[saved])):
+                elem = ET.SubElement(ins,'layer')
+                elem.text = (str(population[saved][layer])[1:-1]).replace(",","")
+            root.append(ins)
+    
+    
+    tree.write(file_Path)
+
+
+
+    
         
 def main():
     for ins in range(0,populationSize):    
-        population.append(createNewGene()) #make the babies
-    
+        population.append(0) #make the babies
+    population[0] = createNewGene()    
+    loadPrev()
+
     for num in range(0,generationNum):
         generation()
-        print(evaluation)
-    
-    
+        print(num)
+    print(evaluation)
+    saveCur()
+
+def DuplicateSeparateList(startList):
+    newList = []
+    for entry in startList:
+        newList.append(entry)
+    return newList
+
 def generation():
     global population
     global evaluation
-    for ins in range(0,populationSize): #fill in population gaps with mutated instances
+    
+    for ins in range(populationSize): #fill in population gaps with mutated instances
         if population[ins] == 0:
             check = ins
             parentNotFound = True
             while parentNotFound:
-                
                 if population[check] == 0:
                     check = random.randint(0,populationSize-1)
                 else:
-                    population[ins] = createGenes(population[check])
+                    
+                    population[ins] = createGenes(copy.deepcopy(population[check]))
                     parentNotFound = False
-          
+    
+    
     evaluation = []
     for ins in range(0,populationSize): # evaluate each member in the population to see how effective it is
-        evaluation.append([fittness(ins,InOut),ins])
+        evaluation.append([fittness(ins),ins])
         
     evaluation = sorted(evaluation, key=operator.itemgetter(0),reverse=True)
     
     
-    
-    for cull in range(round( populationSize * (fitnessReq)),populationSize):
+    standard = int( populationSize * (fitnessReq))
+    if standard < 1:
+        standard = 1
+    for cull in range(standard,populationSize):
         population[evaluation[cull][1]] = 0
+        evaluation[cull] = 0
+    
             
-            
-def fittness(ins,InOut):
+def fittness(ins):
+    global InOut
     value = 0
     for Q in range(0,len(InOut)):
         output = decision(population[ins],InOut[Q][0])
@@ -143,4 +217,5 @@ def fittness(ins,InOut):
     return value / len(InOut)
 
 main()
+
 
